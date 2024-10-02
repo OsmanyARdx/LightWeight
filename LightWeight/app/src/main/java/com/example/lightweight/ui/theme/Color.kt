416ff5556e66f9eeb5ce56ba45f600f1716563f0
OsmanyARdx <osmarguet@gmail.com>
2024-10-02 16:25:27 -0400

    Color Palette for the theme

--- LightWeight/app/src/main/java/com/example/lightweight/ui/theme/Color.kt
+++ LightWeight/app/src/main/java/com/example/lightweight/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+//LightWeight Color Palette
+val limeGreen = Color(0xFF7DA790)
+val cyanGreen = Color(0xFF1AAD83)
+val blackGreen = Color(0xFF0E100F)
+val moderateGreen = Color(0xFF4FA27F)
+val grayishPink = Color(0xFFF9F7F8)
+val softGreen = Color(0xFF7BE3BF)
+val saturatedGreen = Color(0xFF1C3833)
+val grayishRed = Color(0xFFF2F0F0)
+val grayishGreen = Color(0xFFF9FAF9)
