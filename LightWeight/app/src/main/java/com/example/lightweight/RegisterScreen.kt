c835a3923af0f71ccec08da13449295bab83820c
Meraj <vipermma253@gmail.com>
2024-10-28 15:01:39 -0400

    Added RegisterScreen.kt -meraj

--- LightWeight/app/src/main/java/com/example/lightweight/RegisterScreen.kt
+++ LightWeight/app/src/main/java/com/example/lightweight/RegisterScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
+
 @Composable
 fun RegisterScreen(onRegistrationSuccess: () -> Unit) {
     var email by remember { mutableStateOf("") }
