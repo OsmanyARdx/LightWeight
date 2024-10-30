420f5e095ab3da9aa5b17620cd2b48622eb11921
aielfa <aielfa@farmingdale.edu>
2024-10-29 20:05:03 -0400

    Push User Info to Database/Hash Password/Pull from Database to Login (#7)
    * Push User Info to Database/Hash Password/Pull from Database to Login
    
    * Fixed previously implemented code that I no longer need

---
+++ LightWeight/app/src/main/java/com/example/lightweight/Utils.kt
@@ -0,0 +1,14 @@
+package com.example.lightweight
+
+
+import java.security.MessageDigest
+
+// Hash password
+fun hashPassword(password: String): String {
+    return try {
+        val bytes = MessageDigest.getInstance("SHA-256").digest(password.toByteArray())
+        bytes.joinToString("") { "%02x".format(it) }
+    } catch (e: Exception) {
+        ""
+    }
+}
